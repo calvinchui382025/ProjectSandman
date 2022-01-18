b870bc6ddf4382ec8e7f1fd1c44a84c00b497e59
Calvin <calvinchui382025@yahoo.com>
2022-01-18 00:22:13 -0600

    refresh working!

--- screens/Stats/index.tsx
+++ screens/Stats/index.tsx
@@ -1,29 +1,52 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, RefreshControl, ScrollView } from 'react-native';
 import { Text, View } from '../../components/Themed';
 import { RootTabScreenProps } from '../../types';
 import CircularProgress from 'react-native-circular-progress-indicator';
 //======================================================
+const generateRandomInteger = () => {
+  const max = 100;
+  const min = 0;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+//======================================================
 export default function Stats({ navigation }: RootTabScreenProps<'TabOne'>) {
 
-  // const [refreshing, setRefreshing] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const [randomValue, setRandomValue] = useState(generateRandomInteger())
 
   // const onRefresh = useCallback(() => {
   //   setRefreshing(true);
   // });
 
+  const wait = (timeout:number) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+  }
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    setRandomValue(generateRandomInteger());
+    wait(2000).then(() => setRefreshing(false));
+  }
+
   return (
-    <View style={styles.container}>
+    <ScrollView 
+      contentContainerStyle={styles.container}
+      refreshControl={<RefreshControl
+        refreshing={refreshing}
+        onRefresh={onRefresh}
+      />}
+      >
       <Text>
         This is the stats screen
       </Text>
       <CircularProgress
         initialValue={0}
-        value={100}
+        value={randomValue}
         radius={60}
         duration={3000}
         textColor={'black'}
-        maxValue={200}
+        maxValue={100}
         title={'MPH'}
         titleColor={'black'}
         titleFontSize={10}
@@ -35,12 +58,11 @@ export default function Stats({ navigation }: RootTabScreenProps<'TabOne'>) {
         inActiveStrokeWidth={10}
         activeStrokeColor={"gold"}
         inActiveStrokeColor={'grey'}
-        // activeStrokeOpacity={1}
         inActiveStrokeOpacity={0.2}
         showProgressValue={true}
         clockwise={true}
       />
-    </View>
+    </ScrollView>
   );
 }
 //======================================================
