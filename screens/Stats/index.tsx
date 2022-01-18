57622a373ada5a876b775ae4f86316151c927f2b
Calvin <calvinchui382025@yahoo.com>
2022-01-18 00:11:00 -0600

    progress bar animation sample

--- screens/Stats/index.tsx
+++ screens/Stats/index.tsx
@@ -1,24 +1,45 @@
-import { StyleSheet } from 'react-native';
-
-// import EditScreenInfo from '../../components/EditScreenInfo';
+import React, { useState, useCallback } from 'react';
+import { StyleSheet, RefreshControl, ScrollView } from 'react-native';
 import { Text, View } from '../../components/Themed';
 import { RootTabScreenProps } from '../../types';
+import CircularProgress from 'react-native-circular-progress-indicator';
 //======================================================
 export default function Stats({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  // const [refreshing, setRefreshing] = useState(false);
+
+  // const onRefresh = useCallback(() => {
+  //   setRefreshing(true);
+  // });
+
   return (
     <View style={styles.container}>
-      {/* <Text style={styles.title}>
-        Tab One
-      </Text> */}
-      {/* <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)" 
-      /> */}
-      {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
       <Text>
         This is the stats screen
       </Text>
+      <CircularProgress
+        initialValue={0}
+        value={100}
+        radius={60}
+        duration={3000}
+        textColor={'black'}
+        maxValue={200}
+        title={'MPH'}
+        titleColor={'black'}
+        titleFontSize={10}
+        titleStyle={{
+          fontWeight: 'bold'
+        }}
+        circleBackgroundColor={'lightgrey'}
+        activeStrokeWidth={15}
+        inActiveStrokeWidth={10}
+        activeStrokeColor={"gold"}
+        inActiveStrokeColor={'grey'}
+        // activeStrokeOpacity={1}
+        inActiveStrokeOpacity={0.2}
+        showProgressValue={true}
+        clockwise={true}
+      />
     </View>
   );
 }
