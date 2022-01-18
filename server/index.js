e4e0d0f99b114fc85981e90e4a152e10786d98fb
Calvin <calvinchui382025@yahoo.com>
2022-01-18 14:17:43 -0600

    route for counter

--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
 const express = require("express");
-// const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-// app.use(cors());
 app.use((_, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //======================================================
 app.get("/api", (req, res) => {
-  console.log('/api hit!'); // test
   res.json({ message: "Successfully hit server!" });
 });
+
+const fakeDatabase = {
+  counter: 0,
+};
+
+app.post("/setCounter", (req, res) => {
+  const value = req?.body?.data?.val;
+  fakeDatabase.counter = value;
+  res.json({ counter: fakeDatabase.counter })
+})
 //======================================================
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
